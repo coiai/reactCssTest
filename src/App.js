6ade1d0f2ffcda6fccbd6958a91b5d8472f20e78
Coiai Chan <coiaichan@gmail.com>
2022-10-02 13:53:11 +0000

    import css module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles.css";
-import { InlineStyle } from "./components/inlineStyle.jsx";
+import { InlineStyle } from "./components/InlineStyle.jsx";
+import { CssModules } from "./components/CssModules.jsx";
 
 export default function App() {
   return (
     <div className="App">
       <InlineStyle />
+      <CssModules />
     </div>
   );
 }
---
+++ src/components/CssModules.jsx
@@ -0,0 +1,10 @@
+import classes from "./CssModules.module.scss";
+
+export const CssModules = () => {
+  return (
+    <div className={classes.container}>
+      <p> css Modules </p>
+      <button> Fignt </button>
+    </div>
+  );
+};
